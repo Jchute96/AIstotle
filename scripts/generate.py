fix: add casual masking and projection layer for training

scripts/generate.py
@@ -40,26 +40,29 @@ def generate(prompt, max_new_tokens=100, temperature=0.1):
     # Convert it to a tensor and use unsqueeze to add a dimension for the batch at position 0
     token_ids = torch.tensor(token_ids).unsqueeze(0).to(device)
     
-    # Generate new tokens for the response
-    for token in range(max_new_tokens):
+    # Make it so that gradients are not tracked during generation since they are not needed
+    with torch.no_grad():
+        # Generate new tokens for the response
+        for token in range(max_new_tokens):
+            
+            # Make sure we only give the model our correct context length
+            context = token_ids[:,-context_length:]
         
-        # Make sure we only give the model our correct context length
-        context = token_ids[:,-context_length:]
+            # Get the predictions
+            predictions = model(context)
         
-        # Get the predictions
-        predictions = model(context)
+            # Get all the scores for the last token and then apply the temperature to them to control randomness
+            last_token_predictions = predictions[:, -1, :] / temperature
         
-        # Get all the scores for the last token and then apply the temperature to them to control randomness
-        last_token_predictions = predictions[:, -1, :] / temperature
+            # Convert to probabilities
+            probabilities = torch.softmax(last_token_predictions, dim=-1)
         
-        # Convert to probabilities
-        probabilities = torch.softmax(last_token_predictions, dim=-1)
+            # Get the next token based off of its probabilities
+            next_token = torch.multinomial(probabilities, num_samples=1)
         
-        # Get the next token based off of its probabilities
-        next_token = torch.multinomial(probabilities, num_samples=1)
-        
-        # Add the token to the sequence
-        token_ids = torch.cat([token_ids, next_token], dim=1)
+            # Add the token to the sequence
+            token_ids = torch.cat([token_ids, next_token], dim=1)
         
     # Decode and return the newly created response
     return tokenizer.decode(token_ids[0].tolist())
+

scripts/model.py
@@ -33,6 +33,11 @@ class AttentionHead(nn.Module):
         # Scale the scores down by dividing by the square root of head_size
         relevance_scores = relevance_scores / (self.head_size ** 0.5)
         
+        # Use masking to prevent tokens from factoring in the scores of tokens after their position
+        sequence_len = relevance_scores.shape[-1]
+        mask = torch.triu(torch.ones(sequence_len, sequence_len,  device=x.device), diagonal=1).bool()
+        relevance_scores = relevance_scores.masked_fill(mask, float('-inf'))
+        
         # Convert relevance scores into probabilities ranging from 0 to 1 that represent how related each token is to the others
         # And use dropout to prevent the model from relying on a single token relationship
         relevance_weights = self.dropout(torch.softmax(relevance_scores, dim=-1))
@@ -55,11 +60,18 @@ class MultiHeadAttention(nn.Module):
         # Create all of the attention heads
         self.heads = nn.ModuleList([ AttentionHead(embedding_dimension, head_size) for head in range(num_heads)])
         
+        # Layer used when we merge heads together so that the model can better learn which heads matter most
+        self.projection = nn.Linear(embedding_dimension, embedding_dimension)
+        
     
     def forward(self, x):
         
         # Pass x to each heads forward method and concatenate the results together
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        x = torch.cat([head(x) for head in self.heads], dim=-1)
+        
+        return self.projection(x)
+    
+
     
 
 class FeedForward(nn.Module):